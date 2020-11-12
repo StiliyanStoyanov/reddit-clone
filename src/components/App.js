60b2b7a3671fd4fed095a340ecd9d0c6d10489c7
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2020-11-12 19:57:31 +0200

    - Setup firebase auth emulator - Update firebase sdk, admin and functions dependencies

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Redirect, Router} from "@reach/router";
 import styled from "@emotion/styled";
 import Navigation from "./Navigtaion/Navigation";
@@ -12,11 +12,10 @@ import {useSessionLogin} from "../hooks/useSessionLogin";
 import CreatePost from "./CreatePost/CreatePost";
 import CreateCommunity from "./CreateCommunity/CreateCommunity";
 import Test from "./Test";
-import {useTestingLogin} from "../hooks/useTestingLogin";
+import {UserPanel} from "./User/UserPanel";
 
 const App = () => {
-    // useSessionLogin();
-    useTestingLogin();
+    useSessionLogin();
     const {user, isLoading} = useUserStore();
     // TODO: Improve loading implementation
     if (isLoading) return <div>Loading</div>;
@@ -26,6 +25,7 @@ const App = () => {
             <PageContainer>
                 <Router>
                     <Posts path="/"/>
+                    <UserPanel path="/user/:username"/>
                     {!user ? <Redirect from="/create-community" to="/login" noThrow/> : <CreateCommunity path="/create-community"/>}
                     {!user ? <Redirect from="/create-post" to="/login" noThrow/> : <CreatePost path="/create-post"/>}
                     {!user && <Login path="/login" />}
--- src/firebase.js
+++ src/firebase.js
@@ -7,11 +7,12 @@ import "firebase/storage"
 firebase.initializeApp(firebaseConfig);
 if (window.location.hostname === 'localhost') {
     console.log("testing locally -- hitting local functions and firestore emulators");
-    firebase.functions().useFunctionsEmulator('http://localhost:5001');
+    firebase.functions().useEmulator('http://localhost', 5001);
     firebase.firestore().settings({
         host: 'localhost:8080',
         ssl: false
     });
+    firebase.auth().useEmulator('http://localhost:9099/');
 }
 export const firestore = firebase.firestore();
 export const auth = firebase.auth();
