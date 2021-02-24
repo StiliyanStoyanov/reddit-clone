6d69a74b81a19983f58b77f1380b44321d8fb6ec
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2021-02-24 16:35:43 +0200

    :construction: Add rendering of single community posts

--- src/components/Posts/Pages/SingleCommunity.js
+++ src/components/Posts/Pages/SingleCommunity.js
@@ -1,16 +1,25 @@
 /** @jsx jsx */
-/* eslint-disable no-unused-vars */
+import React from "react";
 import {css, jsx} from "@emotion/core";
 import {useTheme} from "emotion-theming";
 import {useMatch} from "@reach/router";
+import {useFetchSingle} from "../../../hooks/useFetchSingle";
+import CardView from "../Views/CardView/CardView";
+import CompactView from "../Views/CompactView/CompactView";
 
 const SingleCommunity = ({view, sort}) => {
     const {communityId} = useMatch('/e/:communityId');
+    const {data, fetchNext, communityExists, loading} = useFetchSingle(sort, communityId);
     const theme = useTheme();
+
+    if (loading) return <div>Loading...</div>;
+    if (!communityExists) return <div>Community does not exist</div>
 
     return (
         <div css={container(theme)}>
-            Single Community
+            <button onClick={fetchNext}>Get Next</button>
+            {view === "card" && <CardView posts={data}/>}
+            {view === "compact" && <CompactView posts={data}/>}
         </div>
     );
 };
