0233d9b69ceaae202b675ef96e8f6377786a32e9
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2021-03-14 07:21:02 +0200

    :sparkles: Add hook to detect overflow of an element

--- src/hooks/useOverflow.js
+++ src/hooks/useOverflow.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 
 function isElement(element) {
     return element instanceof Element || element instanceof HTMLDocument;
