ba44a8dd33b6f9d663668064c21221bd7f4acc1c
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2021-03-22 22:02:10 +0200

    :truck: Rename styles declarations :lipstick: Minor updates to styles and added labels

--- src/components/Navigtaion/Dropdown/DropdownToggle.js
+++ src/components/Navigtaion/Dropdown/DropdownToggle.js
@@ -19,47 +19,47 @@ const DropdownToggle = ({icon, open, toggleDropdown, enableFocusLock}) => {
         document.activeElement.blur();
     }
     return (
-        <div css={toggleFocusableContainerStyle(theme)} tabIndex={0} onKeyDown={handleKeyDown} onMouseDown={preventDefault}>
-            <div css={toggleContainerStyle(theme, open)} tabIndex={-1} onMouseDown={handleMouseDown}>
-                <FontAwesomeIcon css={iconCss(theme, open)} icon={icon}/>
-                <div css={overlayStyle(theme)}/>
+        <div css={[divFocusable(theme, open)]} tabIndex={0} onKeyDown={handleKeyDown} onMouseDown={preventDefault}>
+            <div css={[div(theme, open)]} tabIndex={-1} onMouseDown={handleMouseDown}>
+                <FontAwesomeIcon css={[iconCss(theme, open)]} icon={icon}/>
+                <div css={[overlayDiv(theme)]}/>
             </div>
         </div>
-
     )
 }
-const toggleFocusableContainerStyle = theme => css`
+const divFocusable = theme => css`
   border-radius: 50%;
   &:focus {
     background-color: ${theme.nav.hoverOverlay};
   }
+  label: dropdown-toggle-focusable-container
 `
-const toggleContainerStyle = (theme, open) => css`
+const div = (theme, open) => css`
   position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
   height: 40px;
   width: 40px;
-  &:active, &:focus {
-    outline: none;
-  }
+  -webkit-tap-highlight-color: transparent;
   background-color: ${open ? theme.nav.iconContainerActiveBackground : theme.nav.iconContainerBackground};
   cursor:pointer;
   border-radius: 50%;
+  label: dropdown-toggle-nonfocusable-container
 `
 const iconCss = (theme, open) => css`
-  height: 20px;
-  width: 20px;
+  font-size: 16px;
   color: ${open ? theme.nav.iconActiveColor : theme.nav.iconColor};
+  label: dropdown-toggle-icon
 `;
-const overlayStyle = theme => css`
- border-radius: 50%;
- position: absolute;
- width: 100%;
- height: 100%;
- &:hover {
+const overlayDiv = theme => css`
+  border-radius: 50%;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  &:hover {
     background-color: ${theme.nav.hoverOverlay};
- }
+  }
+  label: dropdown-toggle-highlight-overlay
 `;
 export default DropdownToggle
