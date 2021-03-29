f1c3f35fd5620183b9f3ca55fa76412d8c6ff732
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2021-03-29 21:04:10 +0300

    :recycle: Update useClickOutside hook

--- src/hooks/useClickOutside.js
+++ src/hooks/useClickOutside.js
@@ -5,23 +5,22 @@ export const useClickOutside = (elementRef, callback, open) => {
     callbackRef.current = callback;
 
     React.useEffect(() => {
+        if (!elementRef.current || !elementRef.current instanceof HTMLElement || typeof callback != "function" || !open) return;
+
         const handleClickOutside = event => {
             // Check if the referenced element doesn't contain the clicked element (e.target)
             // Meaning the click was outside
-            if (!elementRef?.current.contains(event.target) && callbackRef.current) {
+            if (!elementRef.current.contains(event.target) && callbackRef.current) {
                 callbackRef.current(event);
             }
         }
-        const handleKeyPress = (event) => {
+        const handleKeyPress = event => {
             if (event.key === 'Escape') {
                 callbackRef.current(event);
             }
         }
-        // Only Attach this listener when the dropdown menu is open
-        if (open) {
-            document.addEventListener("mousedown", handleClickOutside);
-            document.addEventListener("keydown", handleKeyPress);
-        }
+        document.addEventListener("mousedown", handleClickOutside);
+        document.addEventListener("keydown", handleKeyPress);
         return () => {
             document.removeEventListener("mousedown", handleClickOutside);
             document.removeEventListener("keydown", handleKeyPress);
