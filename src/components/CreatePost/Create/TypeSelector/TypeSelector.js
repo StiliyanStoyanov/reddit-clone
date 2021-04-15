751960c453767c43dfbb04da1e5c952c8018b526
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2021-04-15 16:54:34 +0300

    :recycle: Add updated create post content type selector

---
+++ src/components/CreatePost/Create/TypeSelector/TypeSelector.js
@@ -0,0 +1,26 @@
+/** @jsxImportSource @emotion/react */
+import {css} from "@emotion/react";
+import {faFileAlt} from "@fortawesome/free-solid-svg-icons/faFileAlt";
+import {faImage} from "@fortawesome/free-solid-svg-icons/faImage";
+import {faLink} from "@fortawesome/free-solid-svg-icons/faLink";
+import Button from "./Button/Button";
+
+const TypeSelector = () => {
+    return (
+        <div css={[container]}>
+            <Button forForm="post" icon={faFileAlt}>Post</Button>
+            <Button forForm="image" icon={faImage}>Image</Button>
+            <Button forForm="link" icon={faLink}>Link</Button>
+        </div>
+    )
+}
+
+const container = css`
+  display: flex;
+  height: 55px;
+  margin-bottom: 12px;
+  border-radius: inherit;
+  label: type-selector-container;
+`
+
+export default TypeSelector
