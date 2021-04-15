c6eaa8c14323ce0026b531376fa14d0ed3226f03
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2021-04-15 17:04:08 +0300

    :recycle: Update app component to include new stores and listeners :fire: Remove some imports for now

--- src/components/App.js
+++ src/components/App.js
@@ -2,46 +2,38 @@
 import React from 'react';
 import {css} from "@emotion/react";
 import Navigation from "./Navigtaion/Navigation";
-import Login from "./Login";
-import SignUp from "./SignUp";
 import {Posts} from "./Posts/Posts";
-import {NotFound} from "./NotFound";
-import {useUserStore} from "../store/UserStoreProvider";
-import {useSessionLogin} from "../hooks/useSessionLogin";
 import CreatePost from "./CreatePost/CreatePost";
 import CreateCommunity from "./CreateCommunity/CreateCommunity";
 import UserSettings from "./UserSettings/UserSettings";
-import {ToastContainer} from "react-toastify";
-import UserNav from "./User/UserNav";
 import {Routes} from "react-router";
+import TestButtonsBar from "../testing/TestButtonsBar";
+import AuthModal from "./shared/AuthModal/AuthModal";
+import Listeners from "./Listeners/Listeners";
+import {SubscriptionsStoreProvider} from "../store/SubscriptionsStoreProvider";
+import {ScoresStoreProvider} from "../store/ScoresStoreProvider";
 
 const App = () => {
-    useSessionLogin();
-    const {isLoading} = useUserStore();
-    // TODO: Improve loading implementation
-    if (isLoading) return <div>Loading</div>;
     return (
         <>
-            <Navigation/>
-            <div css={container}>
-                <Routes>
-                    <Posts path="/*"/>
-                    <CreateCommunity path="/create-community"/>
-                    <CreatePost path="/create-post"/>
-                    <UserSettings path="/settings/*"/>
-                    <UserNav path="/user/*"/>
-                    <Login path="/login"/>
-                    <SignUp path="/register"/>
-                    <NotFound path="*"/>
-                </Routes>
-                <ToastContainer
-                    autoClose={4000}
-                    position="top-center"
-                    draggablePercent={60}
-                    pauseOnHover={false}
-                />
-            </div>
+            <SubscriptionsStoreProvider>
+                <ScoresStoreProvider>
+                    <div css={container}>
+                        <Navigation/>
+                        <TestButtonsBar/>
+                        <Routes>
+                            <Posts path="/*"/>
+                            <CreateCommunity path="/create-community"/>
+                            <CreatePost path="/create-post"/>
+                            <UserSettings path="/settings/*"/>
+                        </Routes>
+                        <AuthModal/>
+                    </div>
+                    <Listeners/>
+                </ScoresStoreProvider>
+            </SubscriptionsStoreProvider>
         </>
+
     );
 }
 const container = css`
@@ -50,5 +42,4 @@ const container = css`
     padding: 0
   }
 `
-
 export default App;
