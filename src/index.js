a5b53551a677773771568486f6e3b94c50235b68
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2021-04-19 21:57:47 +0300

    :recycle: Add auth modal provider

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import App from './components/App';
 import {BrowserRouter} from "react-router-dom";
 import {UserStoreProvider} from "./store/UserStoreProvider";
 import {ThemeProvider} from "./store/ThemeProvider";
+import {AuthModalStoreProvider} from "./store/AuthModalStoreProvider";
 
 ReactDOM.render(
-    <UserStoreProvider>
-        <ThemeProvider>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </ThemeProvider>
-    </UserStoreProvider>,
+    <BrowserRouter>
+        <UserStoreProvider>
+            <ThemeProvider>
+                <AuthModalStoreProvider>
+                    <App/>
+                </AuthModalStoreProvider>
+            </ThemeProvider>
+        </UserStoreProvider>
+    </BrowserRouter>,
     document.getElementById('root')
 );
