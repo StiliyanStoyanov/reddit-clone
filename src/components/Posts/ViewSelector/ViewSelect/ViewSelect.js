6df26982cc2ea95fca1e8a3e3d21e2da2d386bea
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2021-04-21 20:52:21 +0300

    :recycle: Refactor view select

--- src/components/Posts/ViewSelector/ViewSelect/ViewSelect.js
+++ src/components/Posts/ViewSelector/ViewSelect/ViewSelect.js
@@ -1,79 +1,86 @@
 /** @jsxImportSource @emotion/react */
-import {css, useTheme} from "@emotion/react";
+import {css} from "@emotion/react";
 import {useRef, useState} from "react";
 import {faCaretDown} from "@fortawesome/free-solid-svg-icons/faCaretDown";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import Button from "./Items/Button";
 import {useClickOutside} from "../../../../hooks/useClickOutside";
 import {ReactComponent as CardSvg} from "../../../../assets/card.svg";
 import {ReactComponent as CompactSvg} from "../../../../assets/compact.svg";
-import Svg from "./Items/SvgIcon";
+import {view_button, view_icon_primary, view_icon_secondary} from "../view_selector_styles";
 
 const ViewSelect = ({view, setView}) => {
-    const theme = useTheme();
     const [visible, setVisible] = useState(false);
-    const viewDropdownContainerRef = useRef();
+    const topContainer = useRef();
     const closeDropdown = e => setVisible(false);
     const changeView = (view, e) => {
         setView(view);
-        closeDropdown();
+        closeDropdown(e);
     }
 
-    useClickOutside(viewDropdownContainerRef, closeDropdown, visible);
+    useClickOutside(topContainer, closeDropdown, visible);
     return (
-        <div css={container} ref={viewDropdownContainerRef}>
-            <div>
-                <button css={dropdownToggleStyle} onClick={() => setVisible(prevState => !prevState)}>
-                    {view === 'card' && <Svg Svg={CardSvg}/>}
-                    {view === 'compact' && <Svg Svg={CompactSvg}/>}
-                    <FontAwesomeIcon css={css`color: ${theme.itemActive}; margin-left: 2px`} icon={faCaretDown}/>
+        <div css={[container]} ref={topContainer}>
+            <button css={dropdownToggle} onClick={() => setVisible(prevState => !prevState)}>
+                {view === 'card' && <CardSvg css={view_icon_primary}/>}
+                {view === 'compact' && <CompactSvg css={view_icon_primary}/>}
+                <FontAwesomeIcon css={iconCss} icon={faCaretDown}/>
+            </button>
+            <div css={[dropdownContainer, visible && {visibility: 'visible'}]}>
+                <button css={view_button} onClick={(e) => changeView('card', e)}>
+                    <CardSvg css={[view_icon_secondary]}/>
+                    <span>Card</span>
                 </button>
-            </div>
-            <div css={dropdownContainer(theme,visible)}>
-                <Button onClick={(e) => changeView('card', e)}>
-                    <Svg Svg={CardSvg} secondary={true}/>
-                    Card
-                </Button>
-                <Button onClick={(e) => changeView('compact', e)}>
-                    <Svg Svg={CompactSvg} secondary={true}/>
-                    Compact
-                </Button>
+                <button css={view_button} onClick={(e) => changeView('compact', e)}>
+                    <CompactSvg css={view_icon_secondary}/>
+                    <span>Compact</span>
+                </button>
             </div>
         </div>
     );
 };
 
-const dropdownToggleStyle = theme => css`
+const dropdownToggle = theme => css`
   background-color: transparent;
   display: flex;
   align-items: center;
   border: 0;
   cursor: pointer;
+
   &:hover, &:focus-visible {
-    background-color: ${theme.itemHighlightBackground};
+    background-color: ${theme.background2};
   }
+
   border-radius: 20px;
   padding: 8px;
+  label: view-select-toggle
 `
 const container = css`
- display: flex;
- position: relative;
- margin-left: auto;
- align-items: center;
+  display: flex;
+  position: relative;
+  margin-left: auto;
+  align-items: center;
+  label: view-select
 `
-const dropdownContainer = (theme, visible) => css`
+const dropdownContainer = theme => css`
   display: flex;
   z-index: 3;
   flex-direction: column;
-  visibility: ${visible ? 'visible' : 'hidden'};
+  visibility: hidden;
   position: absolute;
   top: 50px;
   right: 0;
-  border: 1px solid ${theme.borderColor};
+  border: 1px solid ${theme.border1};
   border-radius: 4px;
   box-shadow: 0 2px 4px 0 rgba(215, 218, 220, 0.2);
   align-items: center;
-  background-color: ${theme.nav.backgroundColor};
+  background-color: ${theme.background1};
+  label: view-select-options
+`
+
+const iconCss = theme => css`
+  color: ${theme.colorHighlight2};
+  margin-left: 2px;
+  label: view-select-icon
 `
 
 export default ViewSelect;
