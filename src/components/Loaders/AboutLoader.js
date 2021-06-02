30009decd4dd59713ad23617c2cee55fe197950f
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2021-06-02 15:01:16 +0300

    :recycle: Update about loader

--- src/components/Loaders/AboutLoader.js
+++ src/components/Loaders/AboutLoader.js
@@ -1,68 +1,63 @@
 /** @jsxImportSource @emotion/react */
 import {css} from "@emotion/react";
-import {animation} from "./loadersAnimation";
+import {animation} from "./styles/animation_style";
 
 const AboutLoader = () => {
     return (
-        <div css={[mainContainer]}>
-            <div css={[animation, headerBox]}/>
-            <div css={[imageTitleContainer]}>
-                <div css={[animation, imageBox]}/>
-                <div css={[animation, titleBox]}/>
+        <div css={[main_container]}>
+            <div css={[animation, header_box]}/>
+            <div css={[image_title_container]}>
+                <div css={[animation, image_box]}/>
+                <div css={[animation, title_box]}/>
             </div>
-            <div css={[animation, aboutBox]}/>
-            <div css={[animation, bottomBox]}/>
+            <div css={[animation, about_box]}/>
+            <div css={[animation, bottom_box]}/>
             <div css={[animation, bottomButtonBox]}/>
         </div>
     );
 }
-const mainContainer = theme => css`
+const main_container = theme => css`
   background-color: ${theme.backgroundLoader};
   padding: 12px;
-  max-width: 300px;
-  width: 100%;
+  width: 350px;
   border-radius: 4px;
   label: loader-container
 `
-const imageTitleContainer = css`
+const image_title_container = css`
   display: flex;
   margin-bottom: 8px;
   align-items: center;
   label: image-title-container
 `
-const headerBox = css`
-  max-width: 200px;
+const header_box = css`
   height: 40px;
-  width: 100%;
+  width: 60%;
   margin-bottom: 12px;
   label: -header-box;
 `
-const imageBox = css`
+const image_box = css`
   width: 54px;
   height: 54px;
   margin-right: 8px;
   border-radius: 50%;
   label: -image-box;
 `
-const titleBox = css`
-  max-width: 110px;
-  width: 100%;
+const title_box = css`
+  width: 35%;
   height: 22px;
   margin-bottom: 8px;
   label: -title-box;
 `
-const aboutBox = css`
+const about_box = css`
   height: 80px;
   width: 100%;
   margin-bottom: 8px;
   label: -about-box;
 `
-
-const bottomBox = css`
+const bottom_box = css`
   height: 25px;
   margin: 4px 0;
-  max-width: 200px;
-  width: 100%;
+  width: 60%;
   label: -bottom-box;
 `
 const bottomButtonBox = css`
