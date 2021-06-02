d304a4df9d4ca6d1a42e74d7a2e1c041c68c0810
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2021-06-02 15:14:28 +0300

    :sparkles: Add compact and card loaders base styles

---
+++ src/components/Loaders/styles/loader_styles.js
@@ -0,0 +1,13 @@
+import {css} from "@emotion/react";
+
+export const compact_box_base = css`
+  height: 20px;
+  margin: 4px;
+  border-radius: 4px;
+`;
+
+export const card_box_base = css`
+  height: 20px;
+  margin: 8px 0;
+  border-radius: 4px;
+`
