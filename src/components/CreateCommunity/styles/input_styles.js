7284327397cfa2f40372b7509b37ae769ea745b3
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2021-06-02 14:38:34 +0300

    :truck: Move community input field styles

---
+++ src/components/CreateCommunity/styles/input_styles.js
@@ -0,0 +1,17 @@
+import {css} from "@emotion/react";
+
+export const input_field_base = (theme) => css`
+  display: block;
+  background-color: ${theme.background1};
+  color: ${theme.color1};
+  outline: none;
+  border: 1px solid ${theme.border1};
+  font-size: 18px;
+  height: 40px;
+  border-radius: 4px;
+  width: 100%;
+  margin-top: 5px;
+  &:focus {
+    border-color: ${theme.colorHighlight1};
+  }
+`
