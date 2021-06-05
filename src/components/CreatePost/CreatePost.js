16cc0d91c4da214130e5f124416d37bb8a880d53
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2021-06-05 22:28:07 +0300

    :recycle: Make create post page visible for non authenticated user (still requires auth to submit)

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -1,12 +1,8 @@
 /** @jsxImportSource @emotion/react */
-import {useUserStore} from "../../store/UserStoreProvider";
 import {create_post_header, create_post_container} from "./styles/create_post_styles";
 import Create from "./Create/Create";
 
 const CreatePost = () => {
-    const {user} = useUserStore();
-
-    if (!user) return null;
     return (
         <div css={[create_post_container]}>
             <h2 css={[create_post_header]}>Create Post</h2>
