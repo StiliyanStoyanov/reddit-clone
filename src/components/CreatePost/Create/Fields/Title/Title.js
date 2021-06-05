269ea1f868041beae0d473b43390c73e49620a8f
Stiliyan Stoyanov <stiliyan_1993@outlook.com>
2021-06-05 22:25:45 +0300

    :recycle: Swap text area with (https://github.com/Andarist/react-textarea-autosize)

--- src/components/CreatePost/Create/Fields/Title/Title.js
+++ src/components/CreatePost/Create/Fields/Title/Title.js
@@ -1,53 +1,35 @@
 /** @jsxImportSource @emotion/react */
-import {postStoreActionTypes, usePostDispatch, usePostStore} from "../../../../../store/PostStoreProvider";
-import {title_text_area} from "../../../styles/fields_styles";
-import {useRef} from "react";
+import {createPostStoreActions, usePostDispatch, usePostStore} from "../../../../../store/CreatePostStoreProvider";
+import {title_textarea} from "../../../styles/fields_styles";
 import {css} from "@emotion/react";
-
-const {updateTitle} = postStoreActionTypes
-
+import TextareaAutosize from 'react-textarea-autosize';
+const {updateTitle} = createPostStoreActions
 // About the resize
 // https://stackoverflow.com/questions/454202/creating-a-textarea-with-auto-resize
-// Should probably check out https://github.com/Andarist/react-textarea-autosize
+// https://github.com/Andarist/react-textarea-autosize
 
 const Title = () => {
-    const textareaRef = useRef();
     const {title} = usePostStore();
     const postDispatch = usePostDispatch();
-
-    const handleChange = (event) => {
-        postDispatch({type: updateTitle, payload: {userInput: event.target.value}});
-        textareaRef.current.style.height = 'auto';
-        textareaRef.current.style.height = textareaRef.current.scrollHeight + 'px';
-    }
-    const preventEnter = event => {
-        if (event.key === "Enter") event.preventDefault();
-    }
+    const handleChange = (event) => postDispatch({type: updateTitle, payload: {userInput: event.target.value}});
 
     return (
-        <div css={titleContainer}>
-            <textarea
-                ref={textareaRef}
-                maxLength={200}
-                css={[title_text_area]}
-                rows={1}
-                onChange={handleChange}
-                onKeyDown={preventEnter}
-                value={title}
-                name="title"
-                id="title"
+        <div css={title_container}>
+            <TextareaAutosize
+                css={[title_textarea]}
                 placeholder="Title"
+                rows={1}
+                maxLength={200}
+                value={title}
+                id="title"
+                onChange={handleChange}
                 spellCheck={false}
             />
-            <span
-                css={[span]}
-            >
-                {title.length}/200
-            </span>
+            <span css={[span]}>{title.length}/200</span>
         </div>
     );
 }
-const titleContainer = css`
+const title_container = css`
   position: relative;
 `
 const span = theme => css`
