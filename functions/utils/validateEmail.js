c64b59fd6c8071b91387048f338d3914e6886333
Stiliyan Stoyanov <stiliyan_1993@abv.bg>
2021-06-20 19:35:04 +0300

    :fire: Remove files

--- functions/utils/validateEmail.js
+++
@@ -1,4 +0,0 @@
-module.exports = validateEmail = (value) => {
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ //eslint-disable-line
-    return emailRegex.test(value);
-}
--- functions/utils/validateUsername.js
+++
@@ -1,4 +0,0 @@
-module.exports = validateUsername = (value) => {
-    const usernameRegex = /^[a-zA-Z0-9_-]*$/
-    return !usernameRegex.test(value);
-}
