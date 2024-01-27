aa2c67745c328f027d6a4dfa682ca22029ad974d
Stiliyan Stoyanov <stiliyan.stoyanow@gmail.com>
2024-01-27 22:37:11 +0200

    feat: add redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './app/store.ts'
 import App from './app/App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 )
