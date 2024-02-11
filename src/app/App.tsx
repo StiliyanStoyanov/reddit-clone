0ee631deea25c473534df6745d63abcc6eafd278
Stiliyan Stoyanov <stiliyan.stoyanow@gmail.com>
2024-02-11 19:25:35 +0200

    chore: refactor in progress

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  return <></>
+  return (
+    <div id="app">
+      <Routes>
+        <Route
+          path="/"
+          element={<div style={{ color: 'white' }}>Refactor in progress</div>}
+        />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
