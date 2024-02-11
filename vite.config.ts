9e36f2622f61f31ea73f7008149e837812ed380d
Stiliyan Stoyanov <stiliyan.stoyanow@gmail.com>
2024-02-11 19:37:45 +0200

    feat: add path aliases - https://vitejs.dev/config/shared-options.html#resolve-alias - https://stackoverflow.com/questions/66043612/vue3-vite-project-alias-src-to-not-working

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'node:path'
 
+// https://stackoverflow.com/questions/66043612/vue3-vite-project-alias-src-to-not-working
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      '@assets': path.resolve(__dirname, './src/assets/'),
+      '@common': path.resolve(__dirname, './src/common/')
+    }
+  },
+  plugins: [react()]
 })
