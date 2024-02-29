5ab8448fc8a215d181d9cd908c47491cd4edbc24
Stiliyan Stoyanov <stiliyan.stoyanow@gmail.com>
2024-02-29 21:26:42 +0200

    feat: add, move and confiugure svg icons

---
+++ src/assets/svg/index.tsx
@@ -0,0 +1,13 @@
+import Logo from './logo.svg?react'
+import Arrow from './arrow.svg?react'
+import Card from './card.svg?react'
+import Compact from './compact.svg?react'
+import Person from './person.svg?react'
+import Add from './add.svg?react'
+import Moon from './moon.svg?react'
+import Login from './login.svg?react'
+import Groups from './groups.svg?react'
+import Close from './close.svg?react'
+import Edit from './edit.svg?react'
+
+export { Logo, Arrow, Card, Compact, Person, Add, Moon, Login, Groups, Close, Edit }
