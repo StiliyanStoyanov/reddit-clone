9ffd8174759ec4faaefc16e92df8980e4facc8e9
Stiliyan Stoyanov <stiliyan.stoyanow@gmail.com>
2024-03-04 11:10:07 +0200

    feat: add reducer paths and api middleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { api } from '../features/api/api';
+import { configureStore } from '@reduxjs/toolkit'
+import { api } from '@features/api/api'
+import themeReducer from '@features/theme/themeSlice'
+import modalReducer from '@/features/modals/modalSlice'
 
 // https://redux-toolkit.js.org/rtk-query/overview#configure-the-store
 export const store = configureStore({
-	reducer: {
-		[api.reducerPath]: api.reducer,
-	},
+  reducer: {
+    [api.reducerPath]: api.reducer,
+    theme: themeReducer,
+    modalId: modalReducer,
+  },
+  middleware: (buildGetDefaultMiddleware) => buildGetDefaultMiddleware().concat(api.middleware),
+})
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
