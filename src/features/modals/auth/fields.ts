d1c392b922bee1f7071284b1dd40451f43eeb936
Stiliyan Stoyanov <stiliyan.stoyanow@gmail.com>
2024-03-04 11:07:48 +0200

    feat: add fields descriptor

---
+++ src/features/modals/auth/fields.ts
@@ -0,0 +1,32 @@
+import { FormFields } from '../types'
+
+export const fields: FormFields = {
+  username: {
+    id: 'username',
+    name: 'username',
+    type: 'text',
+    label: 'Username',
+    autoComplete: 'username',
+  },
+  email: {
+    id: 'email',
+    name: 'email',
+    type: 'email',
+    label: 'Email',
+    autoComplete: 'email',
+  },
+  password: {
+    id: 'password',
+    name: 'password',
+    type: 'password',
+    label: 'Password',
+    autoComplete: 'new-password',
+  },
+  confirmPassword: {
+    id: 'confirmPassword',
+    name: 'confirmPassword',
+    type: 'password',
+    label: 'Confirm Password',
+    autoComplete: 'new-password',
+  },
+}
