46c4530632ce547f2d77fd2c2bf54b3bb9625984
Stiliyan Stoyanov <stiliyan.stoyanow@gmail.com>
2024-03-04 11:05:29 +0200

    refactor: add routing wrapper and fix imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './app/store.ts'
-import App from './app/App.tsx'
-import './index.css'
+import { store } from '@app/store'
+import App from '@app/App'
+import '@/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
-  </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 )
