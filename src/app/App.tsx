7cf2c9aaba5f28c8853cc26a41978f7fa3cffaca
Stiliyan Stoyanov <stiliyan.stoyanow@gmail.com>
2024-03-16 18:33:39 +0200

    feat: add navigation and modal controller to the app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,15 @@
+import { Modals } from '@/features/modal/Modals'
+import { Navigation } from '@/features/navigation/Navigation'
 import { Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
     <div id="app">
+      <Navigation />
       <Routes>
         <Route path="/" element={<div>Refactor in progress</div>} />
       </Routes>
+      <Modals />
     </div>
   )
 }
