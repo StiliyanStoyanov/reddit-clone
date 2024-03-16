7e1be7382ff1443f7ed98d9ed87b534543ae206d
Stiliyan Stoyanov <stiliyan.stoyanow@gmail.com>
2024-03-16 18:29:39 +0200

    refactor: add navigation index re-export

---
+++ src/features/navigation/index.ts
@@ -0,0 +1,9 @@
+import { Navigation } from './Navigation'
+import { UserDropdown } from './UserDropdown/UserDropdown'
+import { CreateDropdown } from './CreateDropdown/CreateDropdown'
+import { DropdownToggle } from './DropdownToggle'
+import { DropdownIcon } from './DropdownIcon'
+import { DropdownButton } from './DropdownButton'
+import { DropdownLink } from './DropdownLink'
+
+export { Navigation, UserDropdown, CreateDropdown, DropdownToggle, DropdownIcon, DropdownButton, DropdownLink }
