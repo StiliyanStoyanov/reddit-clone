4e3ca3bef11002f82d44b1a908ec9e0c1b031775
Stiliyan Stoyanov <stiliyan.stoyanow@gmail.com>
2024-03-16 18:35:33 +0200

    feat: add user data subscription

--- src/features/api/auth.ts
+++ src/features/api/auth.ts
@@ -1,14 +1,31 @@
+import { auth, db } from '@/config/firebase'
+import { onAuthStateChanged, signOut as _signOut } from 'firebase/auth'
+import { onSnapshot, collection, query, where, Unsubscribe, DocumentData } from 'firebase/firestore'
 import { api } from './api'
 
 const authApi = api.injectEndpoints({
   endpoints: (builder) => ({
-    example: builder.mutation({
-      queryFn: async (args) => {
-        return { data: null }
-      }
-    })
+    authSubscription: builder.query<DocumentData | null, void>({
+      queryFn: () => ({ data: null }),
+      async onCacheEntryAdded(_, { updateCachedData, cacheEntryRemoved }) {
+        let fsUnsub: Unsubscribe | undefined
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+          if (!user) {
+            updateCachedData(() => null)
+            return fsUnsub?.()
+          }
+          const q = query(collection(db, 'users'), where('uid', '==', user.uid))
+          fsUnsub = onSnapshot(q, (userSnapshot) => {
+            updateCachedData(() => userSnapshot.docs[0].data())
+          })
+        })
+        await cacheEntryRemoved
+        unsubscribe()
+      },
+      keepUnusedDataFor: 1,
+    }),
   }),
-  overrideExisting: false
+  overrideExisting: false,
 })
-
-export const { useExampleMutation } = authApi
+export const signOut = () => _signOut(auth)
+export const { useAuthSubscriptionQuery } = authApi
